3c8a029ab1f5dfdd95e2aa6b3994b2159e62f1bd
Hiago Lanzo <hiagolanzo10@gmail.com>
2022-08-25 23:52:57 -0300

    terceiro commit

---
+++ ex030.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+num = int(input('Digite um numero: '))
+if num%2==1:
+    print('IMPAR')
+else:
+    print('PAR')
