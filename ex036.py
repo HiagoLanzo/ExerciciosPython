b4e353eb15ad9e8f2ba6279b50b570696a174e08
Hiago Lanzo <hiagolanzo10@gmail.com>
2022-08-30 13:59:44 -0300

    primeiro commmit

--- ex036.py
+++ ex036.py
@@ -1,13 +1,9 @@
-# Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
-# Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
-# A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
-casa = float(input('Valor da casa: R$ '))
+casa = float(input('Valor casa: R$ '))
 sal = float(input('Salario do comprador: R$ '))
-anos = int(input('Quantos anos de financiamento? '))
-fin = casa / (anos*12)
-print('Para pegar uma casa de R${:.2f} em {} anos, a prestação sera de R${:.2f}'.format(casa, anos, fin))
-if fin > ((30/100)*sal):
-   print('Emprestimo NEGADO!')
+anos = int(input('Em quantos anos de finaciamento? '))
+pres = casa / (anos*12)
+print('Para pegar uma casa de R${} em {} anos, a prestação sera de {:.2f}'.format(casa,anos,pres))
+if pres > (30/100*sal):
+    print('Emprestimo NEGADO!')
 else:
-    print('Emprestimo CONCEDIDO')
-
+    print('Emprestimo CONCEDIDO!')
