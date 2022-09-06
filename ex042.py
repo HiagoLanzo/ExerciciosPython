4075a9dd0d967fd8592422d27f7f87ed8dc9ae33
Hiago Lanzo <hiagolanzo10@gmail.com>
2022-09-05 21:19:50 -0300

    setimo commit

--- ex042.py
+++ ex042.py
@@ -7,7 +7,7 @@ r1 = float(input('Primeiro segmento: '))
 r2 = float(input('Segundo segmento: '))
 r3 = float(input('Terceiro segmento: '))
 if r1 < r2+ r3 and r2 < r1 +r3 and r3 < r1+ r2:
-    print('Os segmentos podem acima podem formar um triangulo1')
+    print('Os segmentos acima podem formar um triangulo1')
     if r1 == r2 == r3:
         print('EQUILATERO')
     elif r1 == r2 or r1 == r3 or r2 == r3:
